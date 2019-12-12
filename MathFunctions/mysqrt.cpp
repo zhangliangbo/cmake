d5d372c710a95ef287fa8244dffac5a728c22228
zhangliangbo <2472110501@qq.com>
2019-12-12 18:15:10 +0800

    sub directory

---
+++ MathFunctions/mysqrt.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 2019/12/12.
+//
+#include <cmath>
+
+double mysqrt(double in) {
+    return sqrt(in);
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include "TutorialConfig.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stdout,"%s Version %d.%d\n",
+                argv[0],
+                Tutorial_VERSION_MAJOR,
+                Tutorial_VERSION_MINOR);
+        fprintf(stdout, "Usage: %s number\n", argv[0]);
+        return 1;
+    }
+    double input = atof(argv[1]);
+    double output = sqrt(input);
+    fprintf(stdout, "The square root of %g\n", input, output);
     return 0;
 }
