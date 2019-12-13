a96945dac3570ba7226968d4c3cb3cf45261a4fa
zhangliangbo <2472110501@qq.com>
2019-12-13 13:35:07 +0800

    ctest测试

--- MathFunctions/mysqrt.cpp
+++ MathFunctions/mysqrt.cpp
@@ -2,7 +2,15 @@
 // Created by Admin on 2019/12/12.
 //
 #include <cmath>
+#include "TutorialConfig.h"
 #include "MathFunctions.h"
+
 double mysqrt(double in) {
-    return in;
+    double result;
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+    result = exp(log(in) * 0.5);
+#else
+    result= in;
+#endif
+    return result;
 }
