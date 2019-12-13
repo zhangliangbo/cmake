fe00d19ae76edecb43a040e21c770f0ec3308014
zhangliangbo <2472110501@qq.com>
2019-12-13 09:38:00 +0800

    sub directory

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <cmath>
 #include "TutorialConfig.h"
 
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        fprintf(stdout,"%s Version %d.%d\n",
+        fprintf(stdout, "%s Version %d.%d\n",
                 argv[0],
                 Tutorial_VERSION_MAJOR,
                 Tutorial_VERSION_MINOR);
@@ -12,7 +16,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     double input = atof(argv[1]);
+#ifdef USE_MYMATH
     double output = sqrt(input);
+#else
+    double output = sqrt(input);
+#endif
     fprintf(stdout, "The square root of %g\n", input, output);
     return 0;
 }
